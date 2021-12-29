8886a16fa1b4b0b2c320e95466c5c458a726c293
tomerp_pepperi <tomer.p@pepperi.com>
2021-12-30 00:04:45 +0200

    - Added project files.

---
+++ client-side/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { PepFileService, PepAddonService } from '@pepperi-addons/ngx-lib';
+
+import { TranslateModule, TranslateLoader, TranslateStore } from '@ngx-translate/core';
+
+import { AppRoutingModule } from './app.routes';
+import { AddonModule } from './addon/addon.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        AddonModule,
+        AppRoutingModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: PepAddonService.createDefaultMultiTranslateLoader,
+                deps: [HttpClient, PepFileService, PepAddonService]
+            }
+        })
+    ],
+    providers: [
+        TranslateStore,
+        // When loading this module from route we need to add this here (because only this module is loading).
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ server-side/my.service.ts
@@ -0,0 +1,27 @@
+import { PapiClient, InstalledAddon } from '@pepperi-addons/papi-sdk'
+import { Client } from '@pepperi-addons/debug-server';
+
+class MyService {
+
+    papiClient: PapiClient
+
+    constructor(private client: Client) {
+        this.papiClient = new PapiClient({
+            baseURL: client.BaseURL,
+            token: client.OAuthAccessToken,
+            addonUUID: client.AddonUUID,
+            addonSecretKey: client.AddonSecretKey,
+            actionUUID: client.AddonUUID
+        });
+    }
+
+    doSomething() {
+        console.log("doesn't really do anything....");
+    }
+
+    getAddons(): Promise<InstalledAddon[]> {
+        return this.papiClient.addons.installedAddons.find({});
+    }
+}
+
+export default MyService;
