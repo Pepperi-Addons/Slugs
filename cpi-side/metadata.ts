7956eed31fcfbbe45697a71f5428f1d2fff6922b
Simcha Waintrob <simcha.w@pepperi.com>
2023-05-04 12:13:45 +0300

    tess

--- cpi-side/metadata.ts
+++ cpi-side/metadata.ts
@@ -1,7 +1,7 @@
 export const legacyPages = [
     '/homepage', // homepage
     
-    '/settings/home', // settings
+    '/settings/home',  // settings
 
     // account dashboard  - already implemented
     '/accounts/home_page/:id', // account dashboard
